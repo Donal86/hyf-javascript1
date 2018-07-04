698285e6c4e5b222e37244427842e2622f1422a3
Donald Zodeh <donald_leb@hotmail.com>
2018-07-05 01:27:50 +0200

    declaring variable x

---
+++ week1/javascript3.js
@@ -0,0 +1,6 @@
+var x;
+console.log('the value of my variable x will be: undefined');
+console.log(x);
+x = 7; // initializing  variable x with an integer.
+console.log('the value of my variable x will be: 7');
+console.log(x);
