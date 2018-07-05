df0aac4fda25f271373a5b9f4859025a98ad01bf
Donald Zodeh <donald_leb@hotmail.com>
2018-07-05 02:12:29 +0200

    Arrays

---
+++ week1/javascript6.js
@@ -0,0 +1,7 @@
+var myArrays = [];
+console.log("the value of the array is []");
+console.log(myArrays);
+var myAnimals = ["parrot", "lion", "elephant", "dog"];
+console.log(myAnimals);
+myAnimals.push("tiger");
+console.log(myAnimals);
